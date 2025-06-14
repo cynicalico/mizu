27760bdff206c5ffb43fa1fc47122ca85bb13b4d
cynicalico <93552084+cynicalico@users.noreply.github.com>
2025-06-14 11:02:34 -0700

    Working memusage on linux

--- src/mizu/util/memusage.cpp
+++ src/mizu/util/memusage.cpp
@@ -6,18 +6,29 @@
 #include <windows.h>
 #include <psapi.h>
 // clang-format on
+#else
+#include <cstdio>
+#include <sys/resource.h>
+#include <unistd.h>
 #endif
 
 namespace mizu {
-#if defined(MIZU_PLATFORM_WINDOWS)
 double memusage() {
+#if defined(MIZU_PLATFORM_WINDOWS)
     PROCESS_MEMORY_COUNTERS_EX pmc;
     GetProcessMemoryInfo(GetCurrentProcess(), reinterpret_cast<PROCESS_MEMORY_COUNTERS *>(&pmc), sizeof(pmc));
     return pmc.PrivateUsage / 1'048'576.0;
-}
 #else
-double memusage() {
-    return 0;
-}
+    long rss = 0L;
+    FILE *fp;
+    if ((fp = fopen("/proc/self/statm", "r")) == nullptr)
+        return 0.0; // Can't open?
+    if (fscanf(fp, "%*s%ld", &rss) != 1) {
+        fclose(fp);
+        return 0.0; // Can't read?
+    }
+    fclose(fp);
+    return static_cast<double>(static_cast<size_t>(rss) * static_cast<size_t>(sysconf(_SC_PAGESIZE))) / 1'048'576.0;
 #endif
+}
 } // namespace mizu
